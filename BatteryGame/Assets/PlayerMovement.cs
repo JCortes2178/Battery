af89c32554f426c737733d1d2e4252384a36acb7
JCortes2178 <44535962+JCortes2178@users.noreply.github.com>
2025-01-25 20:46:38 -0800

    Add horizontal movement to player

--- BatteryGame/Assets/PlayerMovement.cs
+++ BatteryGame/Assets/PlayerMovement.cs
@@ -1,16 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement")]
+    [SerializeField] public float moveSpeed = 5f;
+
+    private Rigidbody playerRigidbody;
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        playerRigidbody.linearVelocity = new Vector2(horizontal * moveSpeed, playerRigidbody.linearVelocity.y);
         
+        if (horizontal > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (horizontal < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
