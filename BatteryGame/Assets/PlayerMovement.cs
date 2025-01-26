cbc4f517d04fea872274455b87cfeb1865ade6f3
JCortes2178 <44535962+JCortes2178@users.noreply.github.com>
2025-01-25 20:57:43 -0800

    Updates horizontal movement

--- BatteryGame/Assets/PlayerMovement.cs
+++ BatteryGame/Assets/PlayerMovement.cs
@@ -1,31 +1,31 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] public float moveSpeed = 5f;
-
-    private Rigidbody playerRigidbody;
+    [SerializeField] public float moveSpeed = 20f;
+    private Rigidbody2D _rigidbody2D;
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        playerRigidbody.linearVelocity = new Vector2(horizontal * moveSpeed, playerRigidbody.linearVelocity.y);
+        _rigidbody2D.linearVelocity = new Vector2(horizontal * moveSpeed, _rigidbody2D.linearVelocity.y);
         
+        //Flip the sprite to match the direction we are facing. Default is right
         if (horizontal > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(10, 10, 1);
         }
 
         if (horizontal < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-10, 10, 1);
         }
+
     }
 }
