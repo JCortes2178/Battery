c8c98a966c005de8b5fadb0fb73c5e2b1e1f42c5
JCortes2178 <44535962+JCortes2178@users.noreply.github.com>
2025-01-27 21:29:09 -0800

    Adds healthbar loss every second

--- BatteryGame/Assets/Scripts/PlayerHealth.cs
+++ BatteryGame/Assets/Scripts/PlayerHealth.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] public float health = 100f;
+    [SerializeField] public float healthLostPerSecond = 1f;
+    [SerializeField] public float dashCost = 5f;
+    [SerializeField] public float attackCost = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = 100;
+        InvokeRepeating("LoseHealthOverTime", 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void LoseHealthOverTime()
+    {
+        health -= healthLostPerSecond;
     }
 }
