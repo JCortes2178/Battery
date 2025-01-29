b7888c035d0f8fb3306d4e917d5e9282e2848b29
JCortes2178 <44535962+JCortes2178@users.noreply.github.com>
2025-01-28 18:04:55 -0800

    Implements a gradient in the healthbar

--- BatteryGame/Assets/Scripts/HealthbarScript.cs
+++ BatteryGame/Assets/Scripts/HealthbarScript.cs
@@ -3,14 +3,19 @@ using UnityEngine.UI;
 public class HealthbarScript : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    
     public void SetMaxHealth(float maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.value = maxHealth;
+        fill.color = gradient.Evaluate(1f); //maxHealth
     }
 
     public void SetHealth(float health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
