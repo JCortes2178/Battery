c0ec2e5cdf9accfceea1745cc21eb178fb946957
amesa <adrian.mesa1798@gmail.com>
2025-01-31 18:26:34 -0500

    Updated Arti Enemy
    updated the enemybullet prefab and the artillery enemy.

--- BatteryGame/Assets/Scripts/Enemy Scripts/EnemyArtillery.cs
+++ BatteryGame/Assets/Scripts/Enemy Scripts/EnemyArtillery.cs
@@ -1,16 +1,28 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyArtillery : Enemy
 {
-    GameObject bulletPrefab;
+    public GameObject player;
+    public GameObject enemyBulletPrefab;
     public Transform firePoint;
+    [SerializeField] private int fireRate;
+    [SerializeField] private float timer;
+
 
     void Update()
     {
-
+        timer += Time.deltaTime;
+        if (timer > fireRate)
+        {
+            timer = 0;
+            Shoot();
+        }
     }
     void Shoot()
     {
-
+        
+        //Vector2 direction = player.transform.position - transform.position;
+        Instantiate(enemyBulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
---
+++ BatteryGame/Assets/Scripts/Enemy Scripts/EnemyBulletScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    public float speed = 20f;
+    [SerializeField] float timeToDissipate = 2f;
+    [SerializeField] float bulletDamage = 100;
+    private GameObject player;
+    public Rigidbody2D _rigidbody2D;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //_rigidbody2D.AddForce(transform.right * speed, ForceMode2D.Impulse); //An alternate way to cause the projectile to move. More affected by the physics engine.
+        Vector2 direction = player.transform.position - transform.position;
+        _rigidbody2D.linearVelocity = new Vector2(direction.x, direction.y).normalized * speed; //Makes projectile move at a fixed rate.
+    }
+    void Update()
+    {
+        Destroy(gameObject, timeToDissipate);
+    }
+
+    //Projectile behavior when it collides.
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        if (hitInfo.CompareTag("Player"))
+        {
+            PlayerHealth playerHP = hitInfo.GetComponentInChildren<PlayerHealth>();
+            if (playerHP != null)
+            {
+                playerHP.LoseHealth(bulletDamage);
+            }else
+            {
+                Debug.LogError("Error: Null Exception in Enemy.cs");
+            }
+        }
+    }
+}
