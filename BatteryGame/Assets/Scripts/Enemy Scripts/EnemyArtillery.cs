12d808820591bd280af58593ac1a88285309ccdf
amesa <adrian.mesa1798@gmail.com>
2025-02-01 17:58:05 -0500

    Created Strafing Enemy and its behavior
    Created a new enemy and gave it its own script based off the turret enemy.

--- BatteryGame/Assets/Scripts/Enemy Scripts/EnemyArtillery.cs
+++ BatteryGame/Assets/Scripts/Enemy Scripts/EnemyArtillery.cs
@@ -6,8 +6,8 @@ public class EnemyArtillery : Enemy
     public GameObject player;
     public GameObject enemyBulletPrefab;
     public Transform firePoint;
-    [SerializeField] private int fireRate;
-    [SerializeField] private float timer;
+    [SerializeField] protected int fireRate;
+    [SerializeField] protected float timer;
 
 
     void Update()
@@ -16,13 +16,14 @@ public class EnemyArtillery : Enemy
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;//Gets the angle in degrees
         firePoint.rotation = Quaternion.Euler(0, 0, rotZ);//returns a rotation based on the line above.
         timer += Time.deltaTime;
+        //create aggrodistance
         if (timer > fireRate)
         {
             timer = 0;
             Shoot();
         }
     }
-    void Shoot()
+    protected void Shoot()
     {
 
         //Vector2 direction = player.transform.position - transform.position;
